665a3403a5da7b525973c71338b7a759d423d6cc
thienpham129 <thienversiopp129.it.vn@gmail.com>
2025-07-03 20:23:53 +0700

    feat: add restart button, add timer counting, add table when not start
    feat: add counting timer
    
    add input point

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -3,7 +3,7 @@ import { PointButton } from "./PointButton";
 export function GameBoard({ points, handleClick }) {
     return (
         // ui board game
-        <div className="w-[600px] h-[400px] bg-white border relative">
+        <div className="w-[600px] h-[400px] bg-white border rounded shadow relative">
             {points.map((p) => (
                 <PointButton key={p.id} id={p.id} x={p.x} y={p.y} onClick={handleClick} />
             ))}
